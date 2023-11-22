805e0de2ad9bb698d44c371a2004883809b88123
MarcoValeri <marcovaleri@hotmail.it>
2023-11-22 22:32:50 +0000

    Add image table

---
+++ src/Entity/Image.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ImageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ImageRepository::class)]
+class Image
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $file = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $descrizione = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFile(): ?string
+    {
+        return $this->file;
+    }
+
+    public function setFile(string $file): static
+    {
+        $this->file = $file;
+
+        return $this;
+    }
+
+    public function getDescrizione(): ?string
+    {
+        return $this->descrizione;
+    }
+
+    public function setDescrizione(?string $descrizione): static
+    {
+        $this->descrizione = $descrizione;
+
+        return $this;
+    }
+}
